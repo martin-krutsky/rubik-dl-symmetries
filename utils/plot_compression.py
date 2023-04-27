e0d7821e8447e721f5e4c2e904f0f74aa4e7b2d3
Martin Krutsky <martin.krutsky@protonmail.com>
2023-04-27 14:44:27 +0300

    finish generation

--- utils/plot_compression.py
+++ utils/plot_compression.py
@@ -2,13 +2,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-def plot_histo(data, filename, bins=50):
+def plot_histo(data, filename, visible_bins=20):
     '''
     plot a histogram of cube distributions.
     '''
     plt.figure(figsize=(10,5))
-    plot = sns.histplot(data, bins=bins)
-    plt.xticks([i for i in range(1, 51, 1)])
+    nr_of_bins = max(data)
+    plot = sns.histplot(data, bins=nr_of_bins)
+    x_ticks = [i for i in range(2, nr_of_bins+1, max(nr_of_bins//visible_bins, 1))]
+    plt.xticks(x_ticks)
     
     plt.savefig(filename)
     plt.show()
